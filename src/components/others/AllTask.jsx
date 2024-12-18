b579591d22abe8c2373a8f4569e33ccf6e480fae
Palak <impalak1118@gmail.com>
2024-12-18 16:43:34 +0530

    AdminDashboard+others

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const AllTask = () => {
   return (
-    <div className='bg-[#1c1c1c] p-5'>
-      <div className='bg-red-400 py-2 px-4 mt-5'>
+    <div id='TaskList' className='bg-[#1c1c1c] rounded p-5 h-48 overflow-auto'>
+      <div className='bg-red-400 mb-2  py-2 px-4 rounded  flex justify-between mt-5'>
+        <h2>Palak</h2>
+        <h3>Make Ui</h3>
+        <h5>status</h5>
+      </div>
+      <div className='bg-green-400 mb-2 py-2 px-4 rounded  flex justify-between mt-5'>
+        <h2>Palak</h2>
+        <h3>Make Ui</h3>
+        <h5>status</h5>
+      </div>
+      <div className='bg-yellow-400 mb-2 py-2 px-4 rounded  flex justify-between mt-5'>
+        <h2>Palak</h2>
+        <h3>Make Ui</h3>
+        <h5>status</h5>
+      </div>
+      <div className='bg-blue-400 mb-2 py-2 px-4 rounded  flex justify-between mt-5'>
+        <h2>Palak</h2>
+        <h3>Make Ui</h3>
+        <h5>status</h5>
+      </div>
+      <div className='bg-purple-400 mb-2 py-2 px-4 rounded  flex justify-between mt-5'>
+        <h2>Palak</h2>
+        <h3>Make Ui</h3>
+        <h5>status</h5>
+      </div>
+      <div className='bg-red-400 mb-2 py-2 px-4 rounded  flex justify-between mt-5'>
         <h2>Palak</h2>
         <h3>Make Ui</h3>
         <h5>status</h5>
--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -27,9 +27,10 @@ const CreateTask = () => {
           <div className="w-2/5 flex flex-col items-start">
             <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
             <textarea className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent  border-[1px] border-gray-400"name="" id="" cols="30" rows="10"></textarea>
+            <button className="text-white bg-emerald-500 py-3 hover:bg-emarald-60 px-5 rounded text-sm mt-4 w-full">Create Task</button>
           </div>
-          <button className="text-white bg-emerald-500 py-3 hover:bg-emarald-60 px-5 rounded text-sm mt-4 w-full">Create Task</button>
         </form>
+         
       </div>
     </div>
   )
