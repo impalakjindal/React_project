3b628627fa88099e6dfd73b171dd30d917673c40
Palak <impalak1118@gmail.com>
2024-12-22 08:39:11 +0530

    remaining changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect } from 'react'
+import React, {useState} from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
 import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 const App = () => {
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage();
+  // useEffect(() => {
+  //   // setLocalStorage()
+  //   getLocalStorage();
 
-  },[])
+  // },[])
+  const [user, setUser] = useState(null)
+  const handleLogin= (email,password)=>{
+    
+  }
   
   return (
     <>
-    <Login /> 
+    {!user?<Login />: ''}
     {/* <EmployeeDashboard />  */}
     {/* <AdminDashboard /> */}
     </>
